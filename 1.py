9047f78692a9d2916e72fb7c0cc00eabbd5db964
ruchirgarg05 <ruchirgarg5@gmail.com>
2021-05-10 14:10:26 +0530

    add trie
    Trie

--- 1.py
+++ 1.py
@@ -23,3 +23,22 @@ for num in nums:
     position=bisect.bisect_left(sortedList, num) # Position where to insert
     bisect.insort(sortedList,num)
     
+####################################################################### Trie ###################################################################################
+_end = '_end_'
+def make_trie(*words):
+    root = dict()
+    for word in words:
+        current_dict = root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict[_end] = _end
+    return root
+def in_trie(trie, word):
+    current_dict = trie
+    for letter in word:
+        if letter not in current_dict:
+            return False
+        current_dict = current_dict[letter]
+    return _end in current_dict
+
+
